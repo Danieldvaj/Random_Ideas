66a25437b602a38162926a580b880c6283b4c2b2
Danieldvaj <102229706+Danieldvaj@users.noreply.github.com>
2023-05-31 08:10:48 +0200

    Create uva6.py

---
+++ uva6.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+def find_files(extension, directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(extension):
+                print(os.path.join(root, file))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py extension directory")
+        sys.exit(1)
+
+    extension = sys.argv[1]
+    directory = sys.argv[2]
+    find_files(extension, directory)
