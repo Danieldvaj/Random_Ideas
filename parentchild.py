7d2dc7494b483ea45f1e4f15999f6c0c954036d9
Danieldvaj <102229706+Danieldvaj@users.noreply.github.com>
2023-07-19 13:10:40 +0200

    Update parentchild.py

--- parentchild.py
+++ parentchild.py
@@ -1,30 +1,33 @@
 import os
 import sys
 
-def open_file(file):
-    if not os.path.isfile(file):
-        print(f"File {file} does not exist.")
+def open_file(file_name):
+    if not os.path.isfile(file_name):
+        print(f"File {file_name} does not exist.")
         sys.exit(1)
     try:
-        with open(file, "r") as file:
+        with open(file_name, "r") as file:
             data = file.readlines()
         return data
     except IOError as e:
-        print(f"Could not read file {file=}: {str(e)}")
+        print(f"Could not read file {file_name}: {str(e)}")
         sys.exit(1)
 
-def parse_data(file):
-    data = open_file(file)
+def parse_data(file_name):
+    data = open_file(file_name)
     processes = dict()
-    for parents in data:
-        if parents.startswith("-"):
-            parent_process = parents
-            processes[parent_process] = None
-        elif not parents.startswith('-'):
-            processes[parent_process] = parents
+    parent_process = None
+    for line in data:
+        line = line.strip()
+        if line.startswith("-"):
+            parent_process = line[1:]
+            processes[parent_process] = []
+        elif parent_process is not None:
+            processes[parent_process].append(line)
+        else:
+            print("Error: Found child process without a parent.")
+            sys.exit(1)
     print(processes)
-
 
 if __name__ == "__main__":
     parse_data("processes.txt")
-
